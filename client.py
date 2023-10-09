8454ea093319882fe209540291f293fc31261bd7
robertoRubilar <r.rubilar02@ufromail.cl>
2023-10-09 14:52:33 -0400

    first commit

---
+++ client.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+username = input("Ingrese un nombre de usuario: ")
+
+host = '127.0.0.1'
+port = 55555
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+def receive_messages():
+ while True:
+     try:
+         message=client.recv(1024).decode('utf-8')
+
+         if message == "@username":
+            client.send(username.encode("utf-8"))
+         else:
+                 print(message)
+     except:
+         print("Error")
+         client.close
+         break
+
+def write_message():
+    while True:
+        message = f"{username}: {input('')}"
+        client.send(message.encode('utf-8'))
+
+recive_thread= threading.Thread(target=receive_messages)
+recive_thread.start()
+
+write_thread= threading.Thread(target=write_message)
+write_thread.start()
---
+++ server.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+
+host = '127.0.0.1'
+port = 55555
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((host,port))
+server.listen()
+print(f"Server andando en {host}:{port}")
+
+
+clients=[]
+usernames=[]
+
+def broadcast(message, _client):
+    for client in clients:
+        if client != _client:
+         client.send(message)
+
+def handle_messages(client):
+ while True:
+     try:
+       message=  client.recv(1024)
+       broadcast(message, client)
+     except:
+       index = clients.index(client)
+       username = usernames[index]
+       broadcast(f"server: {username} ha abandonado la sesion".encode('utf-8'))
+       clients.remove(client)
+       usernames.remove(username)
+       client.close()
+       break
+
+
+def receive_connections():
+    while True:
+       client, adress= server.accept()
+
+       client.send("@username".encode("utf-8"))
+       username= client.recv(1024).decode('utf-8')
+
+       clients.append(client)
+       usernames.append(username)
+
+       print(f"{username} se ha conectado desde {str(adress)}")
+
+       message = f"server: {username} ha ingresado al chat".encode("utf-8")
+       broadcast(message, client)
+       client.send("Conectado al servidor".encode("utf-8"))
+
+       thread= threading.Thread(target=handle_messages, args= (client,))
+       thread.start()
+
+receive_connections()
+
